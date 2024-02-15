c348727a596b1224d93e96d3c01e58156ab6a204
Matheus Sousa <matheussousab2014@gmail.com>
2024-02-15 17:13:13 -0300

    add componentes e paginas

---
+++ barber-frontend/src/components/MainHeader/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import { Crown } from "@phosphor-icons/react";
+import Logo from "../../assets/svg/Logo.svg";
+
+import "./style.css";
+
+export default function MainHeader() {
+
+    const links = [
+        { name: 'Home', link: '/' },
+        { name: 'Serviços e Preços', link: '/servicos' },
+        { name: 'Contato', link: '/contato' },
+        { name: 'Shop', link: '/shop' },
+    ];
+
+    return (
+        <div className="main-header">
+            <img src={Logo} alt="Logo" />
+            <div className="content-links">
+                {links.map((link, i) => (
+                    <NavLink
+                        key={i}
+                        to={link.link}
+                        className={({ isActive, isPending }) =>
+                            isPending ? "link-pending" : isActive ? "link-active" : "link-pending"
+                        }>
+                        {link.name}
+                    </NavLink>
+                ))}
+            </div>
+            <a href="/login" className="button-login-user">
+                <Crown size={22} />
+                Entrar
+            </a>
+        </div>
+    )
+}
