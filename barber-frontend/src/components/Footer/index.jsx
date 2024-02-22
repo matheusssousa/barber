d0627d784b00b4ce5929c64773c75011977412a1
Matheus Sousa <matheussousab2014@gmail.com>
2024-02-22 18:13:29 -0300

    add componentes

--- barber-frontend/src/components/Footer/index.jsx
+++ barber-frontend/src/components/Footer/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
 
 import "./style.css";
 
 export default function Footer(params) {
     return (
         <footer>
-            
+            <p>Desenvolvido por Matheus Sousa</p>
+            <span className="info-icons">
+                <a href="https://www.instagram.com/theusousab/" target="_blank" rel="noopener noreferrer" className="instagram">
+                    <InstagramLogo size={25} />
+                </a>
+                <a href="https://github.com/matheusssousa" target="_blank" rel="noopener noreferrer" className="github">
+                    <GithubLogo size={25} />
+                </a>
+                <a href="https://www.linkedin.com/in/matheus-sousa-17b214208/" target="_blank" rel="noopener noreferrer" className="linkedin">
+                    <LinkedinLogo size={25} />
+                </a>
+            </span>
         </footer>
     )
 }
--- barber-frontend/src/components/MainHeaderAdmin/index.jsx
+++ barber-frontend/src/components/MainHeaderAdmin/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Logo from '../../assets/svg/Logo.svg';
+import { TextAlignLeft } from '@phosphor-icons/react';
+import { useAuth } from '../../providers/AuthProvider';
+import './style.css';
 
 export default function MainHeaderAdmin() {
+    const { admin } = useAuth();
     return (
-        <nav>
-            
-        </nav>
+        <>
+            <div className="navbar-admin">
+                <button type="button"><TextAlignLeft size={32} /></button>
+                <p>ADMINISTRATIVO / <strong className="text-barber_marrom uppercase"></strong></p>
+                <span></span>
+            </div>
+            {/* <nav className="sidebar-admin">
+                <img src={Logo} alt="Logo" />
+            </nav> */}
+        </>
     )
 }
