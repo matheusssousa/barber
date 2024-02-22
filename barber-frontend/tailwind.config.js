75e7174294805be79ace014f1b076db4bc4c30b6
Matheus Sousa <matheussousab2014@gmail.com>
2024-02-22 18:13:42 -0300

    add novas cores

--- barber-frontend/tailwind.config.js
+++ barber-frontend/tailwind.config.js
@@ -7,10 +7,13 @@ export default {
   theme: {
     extend: {
       colors: {
+        barber_preto: '#050505',
         barber_pretocinza: '#121212',
+        barber_cinza: '#141414',
+        barber_cinzaclaro: '#343434',
+        barber_cinzaescuro: '#1E1E1E',
         barber_marrom: '#977656',
         barber_bege: '#F2DAC2',
-        barber_preto: '#050505'
       },
     },
   },
